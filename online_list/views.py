b80f27bcd1c780c4ce2df7a0e0c8175d8ff948ad
BrinGuo <709505649@qq.com>
2018-11-06 13:28:12 +0800

    Update views.py

--- online_list/views.py
+++ online_list/views.py
@@ -9,23 +9,15 @@ import task
 
 
 def online_index(request):
-    host_list=[{'ip':'10.120.116.61','group':'00','appname':'oltp-center','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.62','group':'00','appname':'oltp-befor','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.63','group':'00','appname':'oltp-asy','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.64','group':'00','appname':'oltp-timer','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.65','group':'00','appname':'oltp-account','version':'onlineTest_v6.6_20160709','stat':'online'},
+    host_list=[
+               {'ip':'xxx','group':'00','appname':'xxx','version':'xxx','stat':'online'},
     ]
     #tas_obj = task.Task(request)
     #host_list = tas_obj.handle()
     return  render_to_response('online_list/online_mgr.html',{'host_list':host_list})
     #print host_list
 def online_mgr(request):
-    """host_list=[{'ip':'10.120.116.61','group':'00','appname':'oltp-center','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.62','group':'00','appname':'oltp-befor','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.63','group':'00','appname':'oltp-asy','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.64','group':'00','appname':'oltp-timer','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.65','group':'00','appname':'oltp-account','version':'onlineTest_v6.6_20160709','stat':'online'},
-    ]"""
+
     task_type = request.GET.get("task_type")
     print task_type
     tas_obj = task.Task(request)
@@ -35,13 +27,7 @@ def online_mgr(request):
     return  HttpResponse(json.dumps(host_list))
     #print HttpResponse(json.dumps(host_list))
 def offline_mgr(request):
-    '''
-    host_list=[{'ip':'10.120.116.71','group':'01','appname':'oltp-center','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.72','group':'01','appname':'oltp-befor','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.73','group':'01','appname':'oltp-asy','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.74','group':'01','appname':'oltp-timer','version':'onlineTest_v6.6_20160709','stat':'online'},
-               {'ip':'10.120.116.75','group':'01','appname':'oltp-account','version':'onlineTest_v6.6_20160709','stat':'online'},
-    ]  '''
+
     task_type = request.GET.get("task_type")
     print task_type
     tas_obj = task.Task(request)
